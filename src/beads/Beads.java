654dba74aba9a086f0a854151a8cc3aeaa02316c
alsaddir <alsaddir@sheridancollege.ca>
2019-05-27 12:58:58 -0400

    creat Beads

---
+++ src/beads/Beads.java
@@ -0,0 +1,16 @@
+package beads;
+
+/**
+ *
+ * @author Rich Smith at ZenOfProgramming.com
+ */
+public class Beads 
+{
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        
+    }
+}
